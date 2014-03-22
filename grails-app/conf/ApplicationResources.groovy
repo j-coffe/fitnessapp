653f2e71c197af815bb5ed24ce37066762430db4
dns <dns@ddybin>
2014-03-22 21:23:58 +0400

    теперь у нас календарик

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -2,4 +2,10 @@ modules = {
     application {
         resource url:'js/application.js'
     }
+    
+//        	overrides {
+//		'jquery-theme' {
+//			resource id:'theme', url:'/css/path/to/jquery-ui-1.10.3.custom.css'
+//		}
+//        }
 }
