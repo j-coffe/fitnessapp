77e3a88a915825162c8a53fce12f4a98975cf5ee
asvel <asvel@ASUS>
2014-03-22 21:26:54 +0400

    -

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -2,4 +2,9 @@ modules = {
     application {
         resource url:'js/application.js'
     }
+    /*overrides {
+		'jquery-theme' {
+            resource id:'theme', url:'/css/path/to/jquery-ui-1.8.17.custom.css'
+        }
+    }*/
 }
