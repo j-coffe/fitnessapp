c0fd09f15323ad5f0977e9a915f4c592e43cff9e
fktiby <fktiby@yandex.ru>
2015-03-24 10:17:19 +0300

    Хрень2

--- grails-app/controllers/fitnessapp/ProtocolController.groovy
+++ grails-app/controllers/fitnessapp/ProtocolController.groovy
@@ -22,48 +22,77 @@ class ProtocolController {
         
         def totalList = [];
         for(q in athletes) {
-          // q.athlete.id  - текущий атлет
-          // id - текущая категория
-          def athMap = [:];
-          athMap=[athleteFIO : q.athlete.firstName + " " + q.athlete.secondName + " " + q.athlete.middleName, athleteNum : q.athlete.num];//наименование участника
-          def q1 = AthletePoint.where{athlete == q.athlete && (protocol in protocols)};
-          int max1 = q1.list().point1.max();
-          int min1 = q1.list().point1.min();
-          int max2 = q1.list().point2.max();
-          int min2 = q1.list().point2.min();
+            // q.athlete.id  - текущий атлет
+            // id - текущая категория
+            def athMap = [:];
+            athMap=[athleteFIO : q.athlete.firstName + " " + q.athlete.secondName + " " + q.athlete.middleName, athleteNum : q.athlete.num];//наименование участника
+            def q1 = AthletePoint.where{athlete == q.athlete && (protocol in protocols)};
+            int max1 = q1.list().point1.max();
+            int min1 = q1.list().point1.min();
+            int max2 = q1.list().point2.max();
+            int min2 = q1.list().point2.min();
 
-          //def q1Max = q1.where{point1 == max(point1)};
+            //def q1Max = q1.where{point1 == max(point1)};
           
-          //def q1Min = q1.where{point1 == min(point1)};
+            //def q1Min = q1.where{point1 == min(point1)};
           
-         // def q2Max = q1.where{point2 == max(point2)};
-         // def q2Min = q1.where{point2 == min(point2)};
-          //бежим по протоколам каждого судьи (в данной категории)          
-          for(n in protocols) {
-              // n.id - текущий протокол
-              def query = AthletePoint.where {  athlete == q.athlete && protocol == n}                
-              AthletePoint ap = query.find();//оценки данного атлета
+            // def q2Max = q1.where{point2 == max(point2)};
+            // def q2Min = q1.where{point2 == min(point2)};
+            //бежим по протоколам каждого судьи (в данной категории)          
+            for(n in protocols) {
+                // n.id - текущий протокол
+                def query = AthletePoint.where {  athlete == q.athlete && protocol == n}                
+                AthletePoint ap = query.find();//оценки данного атлета
               
-              int p1 = athMap?.point1 == null ? 0 : athMap?.point1;
-              int p2 = athMap?.point2 == null ? 0 : athMap?.point2;
-              if(ap != null) {
-               athMap.put("point1", p1+ap.point1);
-               athMap.put("point2", p2+ap.point2);
-              } else {
-               athMap.put("point1", p1);
-               athMap.put("point2", p2);
-              }
-          }
+                int p1 = athMap?.point1 == null ? 0 : athMap?.point1;
+                int p2 = athMap?.point2 == null ? 0 : athMap?.point2;
+                if(ap != null) {
+                    athMap.put("point1", p1+ap.point1);
+                    athMap.put("point2", p2+ap.point2);
+                } else {
+                    athMap.put("point1", p1);
+                    athMap.put("point2", p2);
+                }
+            }
   
-          int p1 = athMap?.point1 == null ? 0 : athMap?.point1;
-          int p2 = athMap?.point2 == null ? 0 : athMap?.point2;
-          athMap.put("point1", p1-max1-min1);
-          athMap.put("point2", p2-max2-min2);
+            int p1 = athMap?.point1 == null ? 0 : athMap?.point1;
+            int p2 = athMap?.point2 == null ? 0 : athMap?.point2;
+            athMap.put("point1", p1-max1-min1);
+            athMap.put("point2", p2-max2-min2);
+          
+            if(cat.summarize){
+                if(athMap.point2==0){
+                    athMap.put("summ",athMap.point1+athMap.point2+10_000 );
+                }
+                else{
+                    athMap.put("summ", athMap.point1+athMap.point2);
+                }
+            }
+            else{
+                if(athMap.point2==0){
+                    athMap.put("point2",athMap.point2+10_000 );
+                }
+            }
         
-          totalList.add(athMap);
+            totalList.add(athMap);
         }
-        
-        totalList.sort({a,b -> a.point1 <=> b.point1 ?: a.athleteNum <=> b.athleteNum})
+        if(cat.summarize){
+            totalList.sort({a,b -> a.summ <=> b.summ})
+            for(t in totalList){
+                if((t.summ)>=10_000)
+                t.summ-=10000;
+                println t.summ
+            }
+        }
+        else{
+            totalList.sort({a,b -> a.point2 <=> b.point2?:a.point1 <=> b.point1})
+             for(t in totalList){
+                if((t.point2)>=10_000)
+                t.point2-=10000;
+                println t.point2
+            }
+        }
+    
         
         //Если хотим ограничить параметром из категории
         def numForPass = cat.numForPass;
@@ -71,12 +100,12 @@ class ProtocolController {
         
         
         //println(protocols.collect());
-//         def list = [];
-//        for(i in athletes) {
-//            Athlete ath = i.athlete;
-//             def athMap=[secondName: ath.secondName, firstName: ath.firstName];
-//             list.add(athMap);
-//        }
+        //         def list = [];
+        //        for(i in athletes) {
+        //            Athlete ath = i.athlete;
+        //             def athMap=[secondName: ath.secondName, firstName: ath.firstName];
+        //             list.add(athMap);
+        //        }
         
         
         render totalList as JSON;
@@ -101,7 +130,7 @@ class ProtocolController {
             
         }
         def myShortListOfAthletes = athletes.athlete.sort({it.round1sum}).take(ccategory.numForPass)
-//        println athletes.athlete.sort({it.round1sum}).take(ccategory.numForPass);
+        //        println athletes.athlete.sort({it.round1sum}).take(ccategory.numForPass);
         //
         render (view:"edit",model:[ccategory:ccategory,judges:judges,protocols:protocols,competition:competition,round:round,myshortlistofathletes:myShortListOfAthletes])
     }  
@@ -113,7 +142,7 @@ class ProtocolController {
                     AthletePoint ap=AthletePoint.findById(t.split("_")[1]);
                     ap.point1=v.toInteger();
                     ap.save(flush: true)
-                     //println (ap.id +" "+ ap.point1+" "+t+" "+v)
+                    //println (ap.id +" "+ ap.point1+" "+t+" "+v)
                 }
                 else if(t.split("_")[0]=="pp2"){
                     AthletePoint ap=AthletePoint.findById(t.split("_")[1]);
@@ -121,8 +150,8 @@ class ProtocolController {
                     ap.save(flush: true)
                     //println (ap.id +" "+ ap.point2+" "+t+" "+v)
                 }
-        })
-//        params.category_id=ccategory.id;
+            })
+        //        params.category_id=ccategory.id;
         //println flash.categ  //${flash.categ}
         render (view:"index")
     }
--- grails-app/views/protocol/index.gsp
+++ grails-app/views/protocol/index.gsp
@@ -19,7 +19,7 @@
         $('#list tbody').empty();
         var tr;
             for (i in json){
-            tr += '<tr><td>' + json[i].athleteNum + '</td><td>' + json[i].athleteFIO + '</td><td>' + json[i].point1 + '</td><td>' + json[i].point2 + '</td><tr>';
+            tr += '<tr><td>' + json[i].athleteNum + '</td><td>' + json[i].athleteFIO + '</td><td>' + json[i].point1 + '</td><td>' + json[i].point2 + '</td><td>' + json[i].summ + '</td><td>' + (parseInt(i)+1) + '</td><tr>';
             }
             $('#list tbody').append(tr);
             var pr_link_url = $('.pr_link').attr('href');
@@ -39,7 +39,7 @@
         $('#list tbody').empty();
         var tr;
             for (i in json){
-            tr += '<tr><td>' + json[i].athleteNum + '</td><td>' + json[i].athleteFIO + '</td><td>' + json[i].point1 + '</td><td>' + json[i].point2 + '</td><tr>';
+            tr += '<tr><td>' + json[i].athleteNum + '</td><td>' + json[i].athleteFIO + '</td><td>' + json[i].point1 + '</td><td>' + json[i].point2 + '</td><td>' + json[i].summ + '</td><td>' + (parseInt(i)+1) + '</td><tr>';
             }
             $('#list tbody').append(tr);
             var pr_link_url = $('.pr_link').attr('href');
@@ -63,6 +63,7 @@
         
         <g:link class="pr_link" controller="protocol" action="edit" params="[category_id:'', round:'']">Редактировать протокол 1</g:link>
         <g:link class="pr_link2" controller="protocol" action="edit" params="[category_id:'',round:'']">Редактировать протокол 2</g:link>
+        
 
     </div>
     
@@ -74,6 +75,8 @@
                         <g:sortableColumn property="athleteFIO" title="ФИО Участника" />
                         <g:sortableColumn property="point1" title="Оценка 1й выход" />
                         <g:sortableColumn property="point2" title="Оценка 2й выход" />
+                        <g:sortableColumn property="sum" title="Сумма" />
+                        <g:sortableColumn property="win" title="Место" />
                     </tr></thead><tbody></table>   
         <div id="pager"></div> 
     </div>
